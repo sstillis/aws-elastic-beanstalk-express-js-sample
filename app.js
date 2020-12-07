b0d7eb5be1ef551f26bd83c2f95182bd437d44a3
Scott <scott@stillisano.com>
2020-12-07 17:38:17 -0500

    changed message again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('What up Biatches!'));
+app.get('/', (req, res) => res.send('I have successfully completed this module! Hell ya!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
